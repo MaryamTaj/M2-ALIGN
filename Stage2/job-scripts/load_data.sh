d8382176bd3758567f6953a17a608df5fd8beb18
MaryamTaj <tajm48822@gmail.com>
2026-06-22 10:52:15 -0400

    Uploading text evaluation results

--- Stage2/job-scripts/load_data.sh
+++ Stage2/job-scripts/load_data.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=48G
-#SBATCH --time=4:00:00
+#SBATCH --time=24:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=maryam.taj@mail.utoronto.ca
