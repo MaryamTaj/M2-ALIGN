76e75e81c34f95a67ece344039568953295e3a41
MaryamTaj <tajm48822@gmail.com>
2026-07-28 11:20:53 -0400

    Removing cached files

--- Misc/evaluation_xnli.sh
+++
@@ -1,3 +0,0 @@
-
-deepspeed run_evaluation.py --task xnli --llm_path LLaMAX/LLaMAX2-7B-XNLI --mt_path google/mt5-xl/ \
-       --init_checkpoint outputs/MindMerger/xnli/augmentation/pytorch_model.bin --augmentation True
